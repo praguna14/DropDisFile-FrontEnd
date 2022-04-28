27c379c8f444a35c8938dce37c8356e58856a552
meetnandu05 <nandu.m@northeastern.edu>
2022-04-27 23:40:42 -0400

    upload and download

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -52,6 +52,58 @@ class HomePage extends React.Component {
         });
     }
 
+    handleDelete = () => {
+        const query = {
+            username: this.props.user.username,
+            filename: this.state.selectedFile
+        }
+        axios.delete('http://localhost:' + this.state.serverIndex + '/delete?username=' + query.username + '&fileName=' + query.filename)
+            .then(response => {
+                console.log('Delete Response: ', response);
+                // let files = this.state.files;
+                // files.splice(files.indexOf(query.filename), 1);
+                // this.setState({ files: files });
+            })
+            .catch(error => {
+                console.log('Error: ', error);
+            });
+    }
+
+    handleDownload = () => {
+        const element = document.createElement('a');
+        var fileSelected = this.state.files.filter(file => file.fileName === this.state.selectedFile);
+        console.log('File Selected: ', fileSelected[0]);
+        const file = new Blob([fileSelected[0].content], { type: 'text/plain' });
+        element.href = URL.createObjectURL(file);
+        element.download = fileSelected[0].fileName;
+        document.body.appendChild(element); // Required for this to work in FireFox
+        element.click();
+    }
+
+    hanldeUpload = () => {
+        var fileName = document.getElementById('fileName').value;
+        var fileContent = document.getElementById('fileContent').value;
+        console.log('Naya file: ', fileName, fileContent);
+        const query = {
+            username: this.props.user.username,
+            fileName: fileName,
+            content: fileContent
+        }
+        axios.post('http://localhost:' + this.state.serverIndex + '/file', query)
+            .then(response => {
+                console.log('Upload Response: ', response);
+                // this.setState({ files: this.state.files, fileName: fileName, fileContent: fileContent });
+                // this.setState({ selectedFile: fileName });
+                axios.get('http://localhost:' + this.state.serverIndex + '/files?username=' + query.username)
+                    .then(response => {
+                        this.setState({ files: response.data });
+                    })
+                    .catch(error => {
+                        console.log('Error: ', error);
+                    });
+            });
+    }
+
     render() {
         const { user, users } = this.props;
         return (
@@ -77,21 +129,26 @@ class HomePage extends React.Component {
                                         </div>
                                     ))
                                 }
-                                {console.log('Selected File: ', this.state.selectedFile)}
-                                {/* {this.state.files.map(file => <li key={file.fileName}>
-                                    {file.fileName}
-                                </li>
-                                )} */}
                             </div>
                         </div>
                     </div>
                     <Row className="mx-0">
-                        <Button as={Col} variant="primary">Upload</Button>{'  '}
-                        <Button as={Col} variant="info" className="mx-2">Download</Button>{'  '}
-                        <Button as={Col} variant="success">Delete</Button>
+                        <Button as={Col} variant="info" className="mx-2" onClick={this.handleDownload}>Download</Button>{'  '}
+                        <Button as={Col} variant="success" onClick={this.handleDelete}>Delete</Button>
                     </Row>
 
-                    <br />
+                    <br/>
+                    <div style={{ borderTop: "2px solid #000 ", marginLeft: 20, marginRight: 20 }}></div>
+                    <br/>
+                    To upload a file, please enter the following details: <br/> <br/>
+                    <Row className="mx-0">
+                        <input type="text" placeholder="Enter file name" id='fileName'/> <br/> <br/> 
+                        <input type="text" placeholder="Enter file content" id='fileContent' /> <br/> <br/>
+                        <Button as={Col} variant="primary" onClick={this.hanldeUpload}>Upload</Button>
+                    </Row>
+                    <br/>
+                    <div style={{ borderTop: "2px solid #000 ", marginLeft: 20, marginRight: 20 }}></div>
+                    <br/>
                     <div className='row'>
                         <p>
                             <Link to="/login">Logout</Link>
